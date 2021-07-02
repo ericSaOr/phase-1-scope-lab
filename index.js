e4a35aee690a0ca85bec8c7936fd59e336c00cfe
Eric Sanchez <ericfsod@gmail.com>
2021-07-02 10:39:56 -0400

    commit tree ffc0461c0fedd4b55df9fe9142a38763a7c13fd3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+var upperCase = customerName.toUpperCase();
+const leastFavoriteCustomer = 'Ernie'
+//arrow function syntax
+//(param1, paramN) => {
+//     let a = 1;
+//     return a + param1 + paramN;
+//  }
+
+// const upperCaseCustomerName = customerName => {
+//     return customerName.toUpperCase;
+// }
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+
+    
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'The Ernies';
+
+
+}
